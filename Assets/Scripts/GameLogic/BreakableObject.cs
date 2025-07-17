a50013116b8e5bdb767f7ac913f90eadb304bc44
minding2796 <minding2796@gmail.com>
2025-07-17 11:42:42 +0900

    add health bar for breakable object

--- Assets/Scripts/GameLogic/BreakableObject.cs
+++ Assets/Scripts/GameLogic/BreakableObject.cs
@@ -1,18 +1,34 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace GameLogic
 {
     public class BreakableObject : Hittable
     {
+        private float _maxHealth;
+        private Vector3[] _positions;
         [SerializeField] private float health;
         [SerializeField] private ObjectParticle brokenObjectParticle;
         [SerializeField] private SpriteRenderer sprite;
         [SerializeField] private BoxCollider2D boxCollider;
-        
+        [SerializeField] private LineRenderer healthBar;
+
+        private void Start()
+        {
+            healthBar.transform.localPosition = new Vector3(0, boxCollider.bounds.extents.y + 0.3f, 0);
+            _positions = new[] {
+                new Vector3(-boxCollider.bounds.extents.x - 0.1f, 0, 0),
+                new Vector3(boxCollider.bounds.extents.x + 0.2f, 0, 0),
+            };
+            _maxHealth = health;
+        }
+
         public override void Hit(int damage = 1)
         {
             health -= damage;
+            _positions[1].x = Mathf.Lerp(-boxCollider.bounds.extents.x - 0.1f, boxCollider.bounds.extents.x + 0.2f, health / _maxHealth);
+            healthBar.SetPositions(_positions);
             if (health > 0) return;
             var op = Instantiate(brokenObjectParticle, transform.position, Quaternion.identity);
             op.transform.localScale = boxCollider.bounds.extents * 2;
